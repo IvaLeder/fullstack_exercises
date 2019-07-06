94e1dea0faf19331475d8b4a20951b428e2a9cb2
Iva <star_forsaken_000@yahoo.co.uk>
2019-07-06 22:17:40 +0200

    Finish exercise 10 for courseinfo

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,34 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = props => <h1>{props.course}</h1>
-
-const Part = props => (
-  <li>
-    {props.part.name} {props.part.exercises}
-  </li>
-)
-
-const Content = ({ parts }) => (
-  <ul>
-    {parts.map(e => (
-      <Part key={e.id} part={e} />
-    ))}
-  </ul>
-)
-
-const Total = props => {
-  const sum = props.parts.reduce((prev, curr) => prev + curr.exercises, 0)
-  return <p>total of {sum}</p>
-}
-
-const Course = props => (
-  <section>
-    <Header course={props.course.name} />
-    <Content parts={props.course.parts} />
-    <Total parts={props.course.parts} />
-  </section>
-)
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
